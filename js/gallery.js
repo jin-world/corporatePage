27b1c9f4aadf073d9e8b2e143be90c171264a52a
goddnsdk37 <goddnsdk37@naver.com>
2021-07-04 20:11:09 +0900

    gallery change-3

--- js/gallery.js
+++ js/gallery.js
@@ -1,50 +1,18 @@
-const url = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos";
-const user = "193201174@N02";
-const key = "9826483353a42d129a03d4cae76a3fb1";
-let num = 20;
+let url = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos";
+// let url_user = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos";
+let url_search = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+let user = "193201174@N02";
+let key = "9826483353a42d129a03d4cae76a3fb1";
+// let num = 20;
 
-//데이터 호출
-$.ajax({
-    url: url,
-    dataType: "json",
-    data: {
-        api_key: key,
-        per_page: num,
-        format: "json",
-        nojsoncallback: 1,
-        user_id: user
-    }
-})
-.success(function(data){
-    console.log(data.photos.photo);
-    var imgs = data.photos.photo;
+getFlickr(url, key, 20);
 
-    $(imgs).each(function(index,data){
-        var tit = data.title;
-        var user_ID = data.user;
-        var imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+//검색버튼
+$(".search button").on("click",function(){
+    var tags = $(this).prev().val(); 
 
-        var imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
-
-        var tags = `
-            <article>
-            <div class="inner">
-                <div class="pic" data-src=${imgSrcBig}>
-                    <img src="${imgSrc}">
-                </div>
-                <h2>${tit}</h2>
-                <p>${user}</p>
-            </div>
-            </article>
-        `;
-
-        $("#gallery").append(tags);
-    })
-})
-.error(function(err){
-    console.log(err);
+    getFlickr(url_search, key, 20, tags);
 });
-
 
 //팝업호출
 $("body").on("click", "#gallery article .pic", function(e){
@@ -63,16 +31,17 @@ $("body").on("click", "#imgPop span", function(e){
 
 //팝업생성 함수정의
 function createPop(imgSrc) {
-    var tags = `
-        <aside id="imgPop">
-            <div class="pic">
-                <img src="${imgSrc}">
-            </div>
-            <span>CLOSE</span>
-        </aside>
-    `;
-
-    $("#gallery").append(tags);
+    $("body")
+        .append(
+            $("<aside id='imgPop'>")
+                .append(
+                    $("<div class='pic'>")
+                        .append(
+                            $("<img>").attr({src : imgSrc}),
+                            $("<span>").text("close")
+                        )
+                )
+        );
     $("#imgPop").fadeIn();
 }
 
@@ -81,4 +50,49 @@ function removePop() {
     $("#imgPop").fadeOut(500,function(){
         $(this).remove();
     })
+}
+
+//데이터 호출 함수정의
+function getFlickr(url, key, num, tags) {
+    $.ajax({
+        url: url,
+        dataType: "json",
+        data: {
+            api_key: key,
+            per_page: num,
+            format: "json",
+            nojsoncallback: 1,
+            tags: tags,
+            user_id: user
+        }
+    })
+    .success(function(data){
+        console.log(data.photos.photo);
+        var imgs = data.photos.photo;
+    
+        $(imgs).each(function(index,data){
+            var tit = data.title;
+            var user_ID = data.user;
+            var imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+    
+            var imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
+    
+            var tags = `
+                <article>
+                <div class="inner">
+                    <div class="pic" data-src=${imgSrcBig}>
+                        <img src="${imgSrc}">
+                    </div>
+                    <h2>${tit}</h2>
+                    <p>${user}</p>
+                </div>
+                </article>
+            `;
+    
+            $("#gallery").append(tags);
+        })
+    })
+    .error(function(err){
+        console.log(err);
+    });
 }
