bad169aa91afd3b3d9d46e4bb81d21e6f9359c7d
goddnsdk37 <goddnsdk37@naver.com>
2021-07-10 16:08:40 +0900

    header gnb

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,24 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 }
 
+//마우스 호버시 2depth 활성화
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
+
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+});
+
+//포커시 이동시 2depth활성화
+$("#gnb>li").each(function(index){
+    //1depth li에서 첫번째 a요소에 focusin이벤트 연결
+    $("#gnb>li").eq(index).find("a").first().on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    });
+
+    //1depth li에서 마지막 a요소에 focusout이벤트 연결
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    });
+})
