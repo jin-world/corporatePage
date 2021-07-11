6e90db6adb9e2c9e10964f735a53944d2c298f21
goddnsdk37 <goddnsdk37@naver.com>
2021-07-12 00:30:13 +0900

    header gnb typeA_change

--- js/custom.js
+++ js/custom.js
@@ -8,24 +8,30 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 }
 
+
+/* GNB메뉴 ------------------------------------------------------------ */
+var $gnb_li = $("#gnb>li");
+
+
 //마우스 호버시 2depth 활성화
-$("#gnb>li").on("mouseenter", function(){
+$gnb_li.on("mouseenter", function(){
     $(this).find(".sub").show();
 });
 
-$("#gnb>li").on("mouseleave", function(){
+$gnb_li.on("mouseleave", function(){
     $(this).find(".sub").hide();
 });
 
+
 //포커시 이동시 2depth활성화
-$("#gnb>li").each(function(index){
+$gnb_li.each(function(index){
     //1depth li에서 첫번째 a요소에 focusin이벤트 연결
-    $("#gnb>li").eq(index).find("a").first().on("focusin", function(){
-        $("#gnb>li").eq(index).find(".sub").show();
+    $gnb_li.eq(index).find("a").first().on("focusin", function(){
+        $gnb_li.eq(index).find(".sub").show();
     });
 
     //1depth li에서 마지막 a요소에 focusout이벤트 연결
-    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
-        $("#gnb>li").eq(index).find(".sub").hide();
+    $gnb_li.eq(index).find("a").last().on("focusout", function(){
+        $gnb_li.eq(index).find(".sub").hide();
     });
 })
