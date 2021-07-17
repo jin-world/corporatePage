cb3499e1eaa6e0ec4f3a7ca638fc8fe0631598b2
goddnsdk37 <goddnsdk37@naver.com>
2021-07-17 12:01:18 +0900

    gallery contentChange

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,9 @@
-let url = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos";
-let url_user = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos";
-let url_search = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
-let user = "193201174@N02";
+//일정기간동안 가장 인기있는 사진을 불러오는 메소드
+let url = "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList";
+//검색한 텍스트를 태그로 가지고 있는 사진 검색하는 메소드
+var url_search = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+// let user = "193201174@N02";
 let key = "9826483353a42d129a03d4cae76a3fb1";
-// let num = 20;
 
 getFlickr(url, key, 20);
 
@@ -28,6 +28,13 @@ $("body").on("click", "#imgPop span", function(e){
     removePop();
 });
 
+//검색 버튼 클릭시
+$(".search button").on("click",function(){
+    //버튼의 앞에 있는 input 태그의 value값을 변수에 담음
+    var tags = $(this).prev().val();
+
+    getFlickr(url_search, key, 40, tags);
+});
 
 //팝업생성 함수정의
 function createPop(imgSrc) {
@@ -63,7 +70,7 @@ function getFlickr(url, key, num, tags) {
             format: "json",
             nojsoncallback: 1,
             tags: tags,
-            user_id: user
+            // user_id: user
         }
     })
     .success(function(data){
@@ -71,6 +78,10 @@ function getFlickr(url, key, num, tags) {
         var imgs = data.photos.photo;
     
         $(imgs).each(function(index,data){
+            //만약 title(사진설명글)이 없을경우 대체 텍스트 처리
+            var text = this.title;
+            if(!this.title) text = "default text";
+
             var tit = data.title;
             var user_ID = data.user;
             var imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
@@ -84,7 +95,7 @@ function getFlickr(url, key, num, tags) {
                         <img src="${imgSrc}">
                     </div>
                     <h2>${tit}</h2>
-                    <p>${user}</p>
+                    <p>${text}</p>
                 </div>
                 </article>
             `;
