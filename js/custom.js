9939be22090660d5ef81cc474e6242fff1c0964d
P김정희\진주 <goddnsdk37@naver.com>
2021-08-02 20:00:18 +0900

    main motion

--- js/custom.js
+++ js/custom.js
@@ -1,72 +1,33 @@
 const btnCall = document.querySelector(".btnCall"); 
 const menuMo = document.querySelector(".menuMo"); 
+var $gnb_li = $("#gnb>li");
 
 btnCall.onclick = function(e){
-    e.preventDefault(); 
+  e.preventDefault(); 
 
-    btnCall.classList.toggle("on");
-    menuMo.classList.toggle("on"); 
+  btnCall.classList.toggle("on");
+  menuMo.classList.toggle("on"); 
 }
 
 
-/* GNB메뉴 ------------------------------------------------------------ */
-var $gnb_li = $("#gnb>li");
-
-
+// GNB menu ------------------------------------------------------------------------------------------
 //마우스 호버시 2depth 활성화
 $gnb_li.on("mouseenter", function(){
-    $(this).find(".sub").show();
+  $(this).find(".sub").show();
 });
 
 $gnb_li.on("mouseleave", function(){
-    $(this).find(".sub").hide();
+  $(this).find(".sub").hide();
 });
 
 
 //포커시 이동시 2depth활성화
 $gnb_li.each(function(index){
-    //1depth li에서 첫번째 a요소에 focusin이벤트 연결
-    $gnb_li.eq(index).find("a").first().on("focusin", function(){
-        $gnb_li.eq(index).find(".sub").show();
-    });
-
-    //1depth li에서 마지막 a요소에 focusout이벤트 연결
-    $gnb_li.eq(index).find("a").last().on("focusout", function(){
-        $gnb_li.eq(index).find(".sub").hide();
-    });
-})
-
-
-//메인 슬라이더
-const swiper = new Swiper("#galleryView", {
-    effect: "coverflow",
-    loop: true,
-    direction:'horizontal',
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50, //슬라이드 회전각
-      stretch: -100, //슬라이더간 거리
-      depth: 180, //깊이 효과값
-      modifier: 1, //효과 배수
-      slideShadows: false, //슬라이더 그림자
-    },
-    // slidesPerView: 4,
-    
-    pagination: {
-      el: ".swiper-pagination",
-      type:'fraction'
-      // clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  $gnb_li.eq(index).find("a").first().on("focusin", function(){
+      $gnb_li.eq(index).find(".sub").show();
   });
-  
-  
+
+  $gnb_li.eq(index).find("a").last().on("focusout", function(){
+      $gnb_li.eq(index).find(".sub").hide();
+  });
+})
---
+++ js/main.js
@@ -0,0 +1,75 @@
+//main visual slide  -------------------------------------------
+const swiper1 = new Swiper("#visual", {
+    loop: true,
+    effect: "fade",
+    speed: 700,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+//Sliding Text Event --------------------------------------------------------------------
+slidingTxt(".txt1", 600, 0);
+slidingTxt(".txt2", 600, 300);
+
+function slidingTxt(el, speed, delay) {
+    var ease = "easeInExpo";
+    var bgColor = $(el).children("span").css("color");
+
+    $(el).append(
+        $("<em class='mask'>")
+            .css({
+                display: "block",
+                width: "100%",
+                height: "100%",
+                backgroundColor: bgColor,
+                position: "absolute",
+                top:0,
+                left:"-100%"
+            })
+    );
+
+    $(el).find(".mask").stop().delay(delay).animate({
+        left: 0
+    }, speed, ease, function(){
+        $(this).prev("span").css({ opacity: 1});
+        $(this).stop().animate({ left : "100%"},speed, ease ,function(){
+            $(this).remove();
+        })
+    });
+}
+
+
+    
+//mainGallery slide  -------------------------------------------
+const swiper = new Swiper("#galleryView", {
+    effect: "coverflow",
+    loop: true,
+    direction:'horizontal',
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    coverflowEffect: {
+    rotate: 50, //슬라이드 회전각
+    stretch: -100, //슬라이더간 거리
+    depth: 180, //깊이 효과값
+    modifier: 1, //효과 배수
+    slideShadows: false, //슬라이더 그림자
+    },
+    
+    pagination: {
+    el: ".swiper-pagination",
+    type:'fraction'
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
